f3ec1cbff3b2f1118343ac5243f8904143d0bf55
Rodrigo Garcia <mysery@gmail.com>
2016-08-06 11:48:03 -0300

    El ejemplo de bullet esta funcionando 100%, para las rotaciones se necesita si o si calcular el momento de inercia, caso contrario el objeto no rotara. Se agregan comentarios y demas. hay que hacer mas ejemplos para ver los funcionamientos y configuraciones que tiene bullet para todos sus body y configs de world. fix #24

--- TGC.Examples/Physics/HelloWorldBullet.cs
+++ TGC.Examples/Physics/HelloWorldBullet.cs
@@ -19,24 +19,20 @@ namespace TGC.Examples.Physics
 
     public class HelloWorldBullet : TGCExampleViewer
     {
-        private TgcPlane floor;
-        private TgcBox box1;
-		private TgcSphere sphere;
-		private TgcBox box3;
+        private TgcPlane floorMesh;
+        private TgcBox boxMesh;
+		private TgcSphere sphereMesh;
 
-
+        //World configurations
         DiscreteDynamicsWorld dynamicsWorld;
         CollisionDispatcher dispatcher;
         DefaultCollisionConfiguration collisionConfiguration;
         SequentialImpulseConstraintSolver constraintSolver;
         BroadphaseInterface overlappingPairCache;
 
-        //shapes:
+        //Rigid Bodies:
         RigidBody floorBody;
-
-        BoxShape boxShape;
         RigidBody boxBody;
-        SphereShape ballShape;
         RigidBody ballBody;
 
         public HelloWorldBullet(string mediaDir, string shadersDir, TgcUserVars userVars, TgcModifiers modifiers)
@@ -44,16 +40,12 @@ namespace TGC.Examples.Physics
         {
             Category = "Physics";
             Name = "Hello world Bullet";
-            Description = "Whoa";
+            Description = "Este ejemplo integra el TGCViewer con el motor de fisica Bullet.";
         }
 
         public override void Init()
         {
-            var floorTexture = TgcTexture.createTexture(D3DDevice.Instance.Device, MediaDir + "Texturas\\granito.jpg");
-            floor = new TgcPlane(new Vector3(-200, 0, -200), new Vector3(400, 0f, 400), TgcPlane.Orientations.XZplane, floorTexture);
-
-            var texture = TgcTexture.createTexture(D3DDevice.Instance.Device, MediaDir + "\\Texturas\\madera.jpg");
-
+            //Creamos el mundo fisico por defecto.
             collisionConfiguration = new DefaultCollisionConfiguration();
             dispatcher = new CollisionDispatcher(collisionConfiguration);
             GImpactCollisionAlgorithm.RegisterAlgorithm(dispatcher);
@@ -64,55 +56,61 @@ namespace TGC.Examples.Physics
                                                         collisionConfiguration);
             dynamicsWorld.Gravity = new BsVector3(0, -10f, 0);
 
+            //Creamos shapes y bodies.
+            
+            //El piso es un plano estatico se dice que si tiene masa 0 es estatico.
             var floorShape = new StaticPlaneShape(new BsVector3(0, 1, 0), 0);
             var floorMotionState = new DefaultMotionState();
-            var floorInfo = new RigidBodyConstructionInfo(0, floorMotionState, floorShape);
+            var floorInfo = new RigidBodyConstructionInfo(0, floorMotionState, floorShape);            
             floorBody = new RigidBody(floorInfo);
             dynamicsWorld.AddRigidBody(floorBody);
 
-            boxShape = new BoxShape(10, 10, 10);
-            var boxTransform = BsMatrix.RotationQuaternion(BsQuaternion.One);
+            //Se crea una caja de tamaño 20 con rotaciones y origien en 10,100,10 y 1kg de masa.
+            var boxShape = new BoxShape(10, 10, 10);
+            var boxTransform = BsMatrix.RotationYawPitchRoll(MathUtil.SIMD_HALF_PI, MathUtil.SIMD_QUARTER_PI, MathUtil.SIMD_2_PI);
             boxTransform.Origin = new BsVector3(10, 100, 10);
             DefaultMotionState boxMotionState = new DefaultMotionState(boxTransform);
+            //Es importante calcular la inercia caso contrario el objeto no rotara.
             BsVector3 boxLocalInertia = boxShape.CalculateLocalInertia(1f);
-            var boxInfo = new RigidBodyConstructionInfo(1f, boxMotionState, boxShape);
+            var boxInfo = new RigidBodyConstructionInfo(1f, boxMotionState, boxShape, boxLocalInertia);
             boxBody = new RigidBody(boxInfo);
             dynamicsWorld.AddRigidBody(boxBody);
 
-            ballShape = new SphereShape(10);
+            //Crea una bola de radio 10 origen 50 de 1 kg.
+            var ballShape = new SphereShape(10);
             var ballTransform = BsMatrix.Identity;
             ballTransform.Origin = new BsVector3(0, 50, 0);
             var ballMotionState = new DefaultMotionState(ballTransform);
+            //Podriamos no calcular la inercia para que no rote, pero es correcto que rote tambien.
             BsVector3 ballLocalInertia = ballShape.CalculateLocalInertia(1f);
             var ballInfo = new RigidBodyConstructionInfo(1, ballMotionState, ballShape, ballLocalInertia);
-            ballInfo.LinearSleepingThreshold = 0.1f;
-            ballInfo.AngularSleepingThreshold = 0.1f;
             ballBody  = new RigidBody(ballInfo);
             dynamicsWorld.AddRigidBody(ballBody);
 
-            var center = new Vector3(0, 0, 0);
-            var size = new Vector3(20, 20, 20);
-            var color = Color.Red;
-            box1 = TgcBox.fromSize(size, texture);
-            sphere = new TgcSphere(1, texture.Clone(), new Vector3(0, 0, 0));
-            sphere.updateValues();
 
-            //Ubicar la camara del framework mirando al centro de este objeto.
-            //La camara por default del framework es RotCamera, cuyo comportamiento es
-            //centrarse sobre un objeto y permitir rotar y hacer zoom con el mouse.
-            //Con clic izquierdo del mouse se rota la cámara, con el derecho se traslada y con la rueda se hace zoom.
-            //Otras cámaras disponibles (a modo de ejemplo) son: FpsCamera (1ra persona) y ThirdPersonCamera (3ra persona).
-            Camara = new TgcRotationalCamera(new Vector3(0, 20, 0), 100, Input);
+            //Cargamos objetos de render del framework.
+            var floorTexture = TgcTexture.createTexture(D3DDevice.Instance.Device, MediaDir + "Texturas\\granito.jpg");
+            floorMesh = new TgcPlane(new Vector3(-200, 0, -200), new Vector3(400, 0f, 400), TgcPlane.Orientations.XZplane, floorTexture);
+
+            var texture = TgcTexture.createTexture(D3DDevice.Instance.Device, MediaDir + "\\Texturas\\madera.jpg");
+            //Es importante crear todos los mesh con centro en el 0,0,0 y que este coincida con el centro de masa definido caso contrario rotaria de otra forma diferente a la dada por el motor de fisica.
+            boxMesh = TgcBox.fromSize(new Vector3(20, 20, 20), texture);
+            //Se crea una esfera de tamaño 1 para escalarla luego (en render)
+            sphereMesh = new TgcSphere(1, texture.Clone(), new Vector3(0, 0, 0));
+            //Tgc no crea el vertex buffer hasta invocar a update values.
+            sphereMesh.updateValues();
+
+            //Ubicar la camara.
+            Camara = new TgcRotationalCamera(new Vector3(0, 50, 0), 200, Input);
         }
 
         public override void Update()
         {
             PreUpdate();
 
+            //Realizamos un step (pueden ser mas internamente) de simulacion.
+            //Es comun utilizar tiempos de simulacion fijos, de esta forma se gana estabilidad y desacoplamiento con el ElapsedTime
             dynamicsWorld.StepSimulation(1/60f, 10);
-            
-                
-                    
 		}
 
         /// <summary>
@@ -128,13 +126,25 @@ namespace TGC.Examples.Physics
             //Iniciamoss la escena
             PreRender();
 
-            DrawText.drawText("boxBody: " + boxBody.MotionState.WorldTransform.ToString(), 5, 20, System.Drawing.Color.Red);
-            DrawText.drawText("ballBody: " + ballBody.MotionState.WorldTransform.ToString(), 5, 40, System.Drawing.Color.Red);
-            DrawText.drawText("boxBody interpolated: " + boxBody.InterpolationWorldTransform.ToString(), 5, 60, System.Drawing.Color.Red);
-            DrawText.drawText("ballBody interpolated: " + ballBody.InterpolationWorldTransform.ToString(), 5, 80, System.Drawing.Color.Red);
+            //Obtenemos la matrix de directx con la transformacion que corresponde a la caja.
+            Matrix m = toDxMatrix(boxBody.InterpolationWorldTransform);
+            //Dibujar las cajas en pantalla
+            boxMesh.Transform = m;
+            boxMesh.render();
+            
+            //Obtenemos la transformacion de la pelota, en este caso se ve como se puede escalar esa transformacion.
+            m = toDxMatrix(ballBody.InterpolationWorldTransform);
+            sphereMesh.Transform = Matrix.Scaling(10, 10, 10) * m;
+            sphereMesh.render();
 
+            floorMesh.render();
 
-            var bs = boxBody.InterpolationWorldTransform;
+            //Finalizamos el renderizado de la escena
+            PostRender();
+        }
+
+        private Matrix toDxMatrix(BsMatrix bs)
+        {
             var m = new Matrix();
             m.M11 = bs.M11;
             m.M12 = bs.M12;
@@ -152,37 +162,7 @@ namespace TGC.Examples.Physics
             m.M42 = bs.M42;
             m.M43 = bs.M43;
             m.M44 = bs.M44;
-            //box1.Transform = Matrix.Identity * m;
-            //Dibujar las cajas en pantalla
-            box1.Transform = m;
-            box1.render();
-            
-            bs = ballBody.InterpolationWorldTransform;
-            m = new Matrix();
-            m.M11 = bs.M11;
-            m.M12 = bs.M12;
-            m.M13 = bs.M13;
-            m.M14 = bs.M14;
-            m.M21 = bs.M21;
-            m.M22 = bs.M22;
-            m.M23 = bs.M23;
-            m.M24 = bs.M24;
-            m.M31 = bs.M31;
-            m.M32 = bs.M32;
-            m.M33 = bs.M33;
-            m.M34 = bs.M34;
-            m.M41 = bs.M41;
-            m.M42 = bs.M42;
-            m.M43 = bs.M43;
-            m.M44 = bs.M44;
-            sphere.Transform = Matrix.Scaling(10, 10, 10) * m;//Matrix.Identity * Matrix.Translation(0, 30f, 0);  
-            //sphere.render();
-            sphere.render();
-
-            floor.render();
-
-            //Finalizamos el renderizado de la escena
-            PostRender();
+            return m;
         }
 
         /// <summary>
@@ -194,18 +174,19 @@ namespace TGC.Examples.Physics
             //Liberar memoria de las cajas 3D.
             //Por mas que estamos en C# con Garbage Collector igual hay que liberar la memoria de los recursos gráficos.
             //Porque están utilizando memoria de la placa de video (y ahí no hay Garbage Collector).
-            box1.dispose();
             dynamicsWorld.Dispose();
             dispatcher.Dispose();
             collisionConfiguration.Dispose();
             constraintSolver.Dispose();
             overlappingPairCache.Dispose();
-            boxShape.Dispose();
             boxBody.Dispose();
-
-            sphere.dispose();
-            ballShape.Dispose();
             ballBody.Dispose();
+            floorBody.Dispose();
+
+            boxMesh.dispose();
+            sphereMesh.dispose();
+            floorMesh.dispose();
+            
         }
     }
 }
